950813edd0cc04492b13166c7156dad95e95a6dc
Francis Pelletier <francispelletier2@hotmail.com>
2022-03-26 12:57:44 -0400

    Modified the slug page to work with mdx files using next-mdx-remote

--- pages/posts/[...slug].js
+++ pages/posts/[...slug].js
@@ -2,17 +2,20 @@ import Layout from "../../components/layout";
 import Head from "next/head";
 import { getPostData, getAllPostSlugArray, getMenuData } from "../../lib/posts";
 import { Heading } from "@chakra-ui/react";
-import ReactMarkdown from "react-markdown";
-import remarkGfm from "remark-gfm";
-import ChakraUIRenderer from "chakra-ui-markdown-renderer";
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote } from "next-mdx-remote";
 
 export async function getStaticProps({ params }) {
   const slug = params.slug.join("/");
   const menuData = getMenuData();
   const postData = await getPostData(slug);
+  const mdxSource = await serialize(postData.fileContents, {
+    parseFrontmatter: true,
+  });
+
   return {
     props: {
-      postData,
+      postData: mdxSource,
       menuData,
     },
   };
@@ -26,22 +29,18 @@ export async function getStaticPaths() {
   };
 }
 
+const components = { h2: Heading };
+
 export default function Post({ postData, menuData }) {
   return (
     <Layout menuData={menuData}>
       <Head>
-        <title>{postData.title}</title>
+        <title>{postData.frontmatter.title}</title>
       </Head>
       <Heading as="h2" size="md" mb={4}>
-        {postData.title}
+        {postData.frontmatter.title}
       </Heading>
-      <ReactMarkdown
-        components={ChakraUIRenderer()}
-        remarkPlugins={[remarkGfm]}
-        skipHtml
-      >
-        {postData.contentHtml}
-      </ReactMarkdown>
+      <MDXRemote {...postData} components={components} />
     </Layout>
   );
 }
