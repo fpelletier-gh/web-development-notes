7b7670cc75bebbb03a88530bb57c2a64945d851a
Francis Pelletier <francispelletier2@hotmail.com>
2022-04-05 15:41:19 -0400

    Deleted unused files

--- components/article.js
+++
@@ -1,9 +0,0 @@
-import styles from "./article.module.css"
-
-export default function Article({children}){
-    return (
-        <article className={styles.article}>
-            {children}
-        </article>
-    )
-}
