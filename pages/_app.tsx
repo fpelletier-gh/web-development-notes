9cef717a694532974633a9139f05c6ed8cc73780
Francis Pelletier <francispelletier2@hotmail.com>
2022-05-24 21:40:24 -0400

    Changed color theme for code block to vsc code dark plus

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../chakra/theme";
-import "../styles/prism-material-dark.css";
+import "../styles/prism-vsc-dark-plus.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
