4e66587a0b639fa2af764e9d66bf4a3c7d265e12
Francis Pelletier <francispelletier2@hotmail.com>
2022-06-01 21:48:42 -0400

    Added a favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import theme from "../chakra/theme";
 import { ColorModeScript } from "@chakra-ui/react";
+import Favicon from "../components/favicon";
 
 export default function Document() {
   return (
@@ -10,6 +11,7 @@ export default function Document() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Roboto+Flex&display=optional"
         />
+        <Favicon />
       </Head>
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
